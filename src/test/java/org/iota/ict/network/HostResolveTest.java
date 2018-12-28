79fd9e1500998d86fe59bee1160a739820c1a887
Lukas Tassanyi <lukas.tassanyi@iota.org>
2018-12-28 15:14:12 +0100

    junit test for #3

---
+++ src/test/java/org/iota/ict/network/HostResolveTest.java
@@ -0,0 +1,35 @@
+package org.iota.ict.network;
+
+import org.junit.Test;
+
+import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author https://github.com/georgmittendorfer (https://github.com/iotaledger/ict/issues/3#issuecomment-450352986)
+ * */
+public class HostResolveTest {
+
+    private final Neighbor neighbor = new Neighbor(new InetSocketAddress("localhost", 42));
+
+    @Test
+    public void sameIpWhenSentPacketFromSameIpThenTrue() {
+        assertTrue("expected match", neighbor.sentPacketFromSameIP(packetFrom("localhost", 42)));
+        assertTrue("expected match", neighbor.sentPacketFromSameIP(packetFrom("localhost", 666)));
+        assertTrue("expected match", neighbor.sentPacketFromSameIP(packetFrom("127.0.0.1", 42)));
+        assertTrue("expected match", neighbor.sentPacketFromSameIP(packetFrom("127.0.0.1", 666)));
+    }
+
+    @Test
+    public void differentIpWhenSentPacketFromSameIpThenFalse() {
+        assertFalse("expected no match", neighbor.sentPacketFromSameIP(packetFrom("google-public-dns-a.google.com", 42)));
+        assertFalse("expected no match", neighbor.sentPacketFromSameIP(packetFrom("8.8.8.8", 42)));
+    }
+
+    private DatagramPacket packetFrom(String hostOrIp, int port) {
+        return new DatagramPacket(new byte[] {}, 0, new InetSocketAddress(hostOrIp, port));
+    }
+
+}
