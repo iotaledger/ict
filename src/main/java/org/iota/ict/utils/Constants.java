60b7469504ea1199458a5d844bd5f5e0ee85296a
Lukas Tassanyi <lukas.tassanyi@iota.org>
2019-01-20 14:41:23 +0100

    change version to 0.5-SNAPSHOT, there won't be a separate 0.4.1

--- src/main/java/org/iota/ict/utils/Constants.java
+++ src/main/java/org/iota/ict/utils/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 
     public static final Path WORKING_DIRECTORY = (new File("./")).toPath();;
 
-    public static final String ICT_VERSION = "0.4.1-SNAPSHOT";
+    public static final String ICT_VERSION = "0.5-SNAPSHOT";
     public static final String ICT_REPOSITORY = "iotaledger/ict";
     public static final String DEFAULT_PROPERTY_FILE_PATH = "ict.cfg";
     public static final String WEB_GUI_PATH = "web/";
