440504c173798e8b06a2e1b088226a8fceae0dfb
Rihards Gravis <rihards.gravis@iota.org>
2019-04-09 13:44:17 +0300

    - Show ICT version in Home page

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Home = () => (
-	<section className="home">
-		<article>
-			<h1>IOTA Controlled Agent</h1>
-		</article>
-	</section>
-);
+import { get } from '../lib/api';
+
+const Home = () => {
+	const [version, setVersion] = useState(null);
+
+	useEffect(async () => {
+		const getConfig = async () => {
+			const info = await get('info');
+			setVersion(info.version);
+		};
+		getConfig();
+	}, []);
+
+	return (
+		<section className="home">
+			<article>
+				<h1>
+					IOTA Controlled Agent
+					{version && (
+						<small>
+							version <strong>{version}</strong>
+						</small>
+					)}
+				</h1>
+			</article>
+		</section>
+	);
+};
 
 export default Home;
