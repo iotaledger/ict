02ce983124c625d3e3ead09a633d1aa4fcbbac6c
Rihards Gravis <rihards.gravis@iota.org>
2019-04-29 12:20:54 +0300

    - Update dependencies - Add log load more on scroll, fixes #63 - Add module send command to `moduleResponse` feature

--- web/src/pages/Log.jsx
+++ web/src/pages/Log.jsx
@@ -1,58 +1,72 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import Card from '../components/Card';
 
 import { get } from '../lib/api';
 import { toDate, downloadFile } from '../lib/helpers';
 
-class Log extends Component {
-	state = {
-		logs: []
+const Log = () => {
+	const [logs, setLogs] = useState([]);
+	const [offset, setOffset] = useState({ max: 0, min: 0 });
+
+	const getLogs = async () => {
+		const params =
+			offset.max > 0
+				? {
+						min: offset.min + logs.length,
+						max: Math.min(offset.max, offset.min + logs.length + 30)
+				  }
+				: null;
+
+		const response = await get('logs', null, params);
+
+		setOffset({ max: response.max, min: response.min });
+		setLogs(logs.concat(response.logs));
 	};
 
-	componentDidMount() {
-		this.init();
-	}
+	useEffect(() => {
+		getLogs();
+	}, []);
 
-	init = async () => {
-		const { logs } = await get('logs');
-		this.setState({ logs });
-	};
-
-	parseLog = () => {
-		const { logs } = this.state;
-		const log = logs.reverse().map(({ timestamp, message }) => `${toDate(timestamp, true)} ${message}`);
+	const parseLog = () => {
+		const log = logs.map(({ timestamp, message }) => `${toDate(timestamp, true)} ${message}`);
 		return log.join('\n');
 	};
 
-	render() {
-		const { logs } = this.state;
-
-		return (
-			<section className="log">
-				<article>
-					<h1>Log</h1>
-					<Card>
-						<nav className="corner">
-							<button
-								onClick={() => downloadFile('ict-log.txt', this.parseLog())}
-								className="button success small"
-								type="button"
-							>
-								Export
-							</button>
-						</nav>
-						{logs.map(({ timestamp, level, message }) => (
-							<p key={`${timestamp}${message}`} className={level.toLowerCase()}>
-								<strong>{toDate(timestamp, true)}</strong>
-								<span>{message}</span>
-							</p>
-						))}
-					</Card>
-				</article>
-			</section>
-		);
-	}
-}
+	return (
+		<section className="log">
+			<article>
+				<h1>Log</h1>
+				<Card>
+					<nav className="corner">
+						<button
+							onClick={() => downloadFile('ict-log.txt', parseLog())}
+							className="button success small"
+							type="button"
+						>
+							Export
+						</button>
+					</nav>
+					{logs.length > 0 && (
+						<InfiniteScroll
+							pageStart={0}
+							loadMore={getLogs}
+							hasMore={logs.length < offset.max - offset.min}
+							loader={<p>Loading ...</p>}
+						>
+							{logs.map(({ timestamp, level, message }, index) => (
+								<p key={`${timestamp}-${index}`} className={level.toLowerCase()}>
+									<strong>{toDate(timestamp, true)}</strong>
+									<span>{message}</span>
+								</p>
+							))}
+						</InfiniteScroll>
+					)}
+				</Card>
+			</article>
+		</section>
+	);
+};
 
 export default Log;
--- web/src/pages/Module.jsx
+++ web/src/pages/Module.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Highlight from 'react-highlight';
 
 import Card from '../components/Card';
 import Popup from '../components/Popup';
@@ -10,11 +11,16 @@ const Module = ({ match }) => {
 	const [config, setConfig] = useState({});
 	const [defaultConfig, setDefault] = useState({});
 	const [request, setRequest] = useState({ error: null, loading: false, shouldConfirm: false });
+	const [command, setCommand] = useState('');
+	const [commandRequest, setCommandRequest] = useState({ error: null, loading: false });
+	const [commandResponse, setResponse] = useState(null);
 
 	const init = async () => {
 		const response = await get('moduleConfig', null, { path: match.params.path });
 		setConfig(response.config);
 		setDefault(response.default_config);
+		setResponse(null);
+		setCommand('');
 	};
 
 	useEffect(() => {
@@ -42,6 +48,34 @@ const Module = ({ match }) => {
 		} else {
 			setRequest({ ...config, error: response.error, loading: false });
 		}
+	};
+
+	const sendCommand = async (e) => {
+		e.preventDefault();
+
+		if (commandRequest.loading) {
+			return;
+		}
+
+		setCommandRequest(Object.assign({}, commandRequest, { loading: true }));
+		setResponse(null);
+
+		const response = await get('ModuleResponse', null, {
+			path: match.params.path,
+			request: command
+		});
+
+		let obj = {};
+
+		try {
+			obj = JSON.parse(response.response);
+			delete obj.success;
+		} catch (e) {
+			obj = { response: response.response };
+		}
+
+		setCommandRequest({ loading: false, error: false });
+		setResponse(JSON.stringify(obj, null, 2));
 	};
 
 	const { error, loading, shouldConfirm } = request;
@@ -76,15 +110,31 @@ const Module = ({ match }) => {
 						{error && <small className="error">{error}</small>}
 					</Popup>
 				)}
-				<form>
-					<Card title={match.params.path} columns>
-						{Object.entries(config).map(([title, value]) => (
-							<label htmlFor={title} key={title}>
-								{title} <input id={title} type="text" onChange={updateEntry(title)} value={value} />
+				<div>
+					{config && (
+						<Card title={match.params.path} columns>
+							{Object.entries(config).map(([title, value]) => (
+								<label htmlFor={title} key={title}>
+									{title} <input id={title} type="text" onChange={updateEntry(title)} value={value} />
+								</label>
+							))}
+						</Card>
+					)}
+					<Card title="Send a command" columns>
+						<form onSubmit={sendCommand}>
+							<label htmlFor="send-command" className="inline">
+								<div>
+									Command
+									<input id="send-command" type="text" onChange={(e) => setCommand(e.target.value)} value={command} />
+								</div>
+								<button className={`button success${commandRequest.loading ? ' loading' : ''}`} type="submit">
+									Send
+								</button>
 							</label>
-						))}
+							{commandResponse && <Highlight language="json">{commandResponse}</Highlight>}
+						</form>
 					</Card>
-				</form>
+				</div>
 			</article>
 		</section>
 	);
